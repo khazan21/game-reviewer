f4c7af11054f2c42bbed70397491b190689c5e59
Robert <khazan21@gmail.com>
2023-08-21 16:09:19 -0500

    worked on my form for reviews

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,17 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../styling/review.css';
+
+const mockReviews = [{
+  id: 1,
+  reviewer_name: 'Robert',
+  review: 'wassup'
+}, {
+  id: 2,
+  reviewer_name: 'Sadaf',
+  review: 'yoooo'
+  },
+  {
+    id: 3,
+    reviewer_name: 'Sadaf',
+    review: 'yoooo'
+  },
+  {
+    id: 3,
+    reviewer_name: 'Sadaf',
+    review: 'yoooo'
+  }];
 
 function Review() {
+
+  const [newReview, setNewReview] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setNewReview('');
+  };
+
   return (
-    <div>
-      <form>
+    <div className='review-list'>
+      <ul className='reviews'>
+        {mockReviews.map(review => (
+          <div key={review.id}>
+            <p>{review.reviewer_name}: {review.review}</p>
+          </div>
+        ))}
+      </ul>
+
+      <form className='form' onSubmit={handleSubmit}>
         <h3>Leave a review!</h3>
 
-        <input 
+        <input
           placeholder='Review'
+          value={newReview}
+          onChange={(event) => setNewReview(event.target.value)}
         />
+
+        <button type="submit">Submit Review</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Review
+export default Review;
