d390d8c07857fce8374eae2ef4b5a702c732c6b1
Robert <khazan21@gmail.com>
2023-08-25 08:34:21 -0500

    Revert "added update and delete button"
    This reverts commit 1cf0875c95c5d12b1ea2d09f68a54583fa05e5c9.

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -4,12 +4,13 @@ import { UserContext } from '../context/user';
 import { useParams } from 'react-router-dom';
 
 function Review() {
-  const { gameId } = useParams();
+
+  const {gameId} = useParams();
+
   const { user } = useContext(UserContext);
-  const [placeholder, setPlaceholder] = useState('Review');
+
   const [game, setGame] = useState(null);
   const [reviews, setReviews] = useState([]);
-  const [newReview, setNewReview] = useState('');
 
   useEffect(() => {
     fetch(`http://127.0.0.1:5555/games/${gameId}`)
@@ -17,11 +18,13 @@ function Review() {
         if (r.ok) {
           r.json().then(game => {
             setGame(game);
-            setReviews(game.reviews);
-          });
+            setReviews(game.reviews)
+          })
         }
-      });
-  }, []);
+      })
+  }, [])
+
+  const [newReview, setNewReview] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -31,7 +34,7 @@ function Review() {
       review: newReview,
       game_id: game.id,
       user_id: user.id
-    };
+    }
 
     fetch('http://127.0.0.1:5555/reviews', {
       method: 'POST',
@@ -40,50 +43,42 @@ function Review() {
     })
       .then(r => {
         if (r.ok) {
-          r.json().then(data => {
-            setReviews([...reviews, data]);
-          });
-        } else {
-          r.json().then(data => {
-            console.log(data);
-          });
+          r.json()
+            .then(data => {
+              setReviews([...reviews, data]);
+            })
         }
-      });
+        else {
+          r.json()
+            .then(data => {
+              console.log(data)
+            })
+        }
+      })
     setNewReview('');
   };
 
   return (
-    <div className='review-container'>
-      <div className='review-list'>
-        <ul className='reviews'>
-          {game && reviews.map(review => (
-            <div className='review' key={review.id}>
-              <button className='deleteBtn' title='Delete'>X</button>
-              <button className='updateBtn' title='Update'>˄</button>
-              <p className='userReview'>{review.reviewer_name}: {review.review}</p>
-            </div>
-          ))}
-        </ul>
+    <div className='review-list'>
+      <ul className='reviews'>
+        {game && reviews.map(review => (
+          <div key={review.id}>
+            <p>{review.reviewer_name}: {review.review}</p>
+          </div>
+        ))}
+      </ul>
 
-        <form className='submitReview' onSubmit={handleSubmit}>
-          <h3>Leave a review!</h3>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>Leave a review!</h3>
 
-          <input
-            className='reviewInput'
-            placeholder={placeholder}
-            style={{ textAlign: 'center' }}
-            value={newReview}
-            onChange={(event) => setNewReview(event.target.value)}
-            onFocus={() => setPlaceholder('')}
-            onBlur={() => {
-              if (newReview === '') {
-                setPlaceholder('Review');
-              }
-            }} />
+        <input
+          placeholder='Review'
+          value={newReview}
+          onChange={(event) => setNewReview(event.target.value)}
+        />
 
-          <button className='submitBtn' disabled={!user} type="submit">Submit Review</button>
-        </form>
-      </div>
+        <button disabled={!user} type="submit">Submit Review</button>
+      </form>
     </div>
   );
 }
