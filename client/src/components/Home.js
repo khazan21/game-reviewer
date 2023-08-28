7a9165b2ffa4708600e6982f2aa86df8d5fd4e23
Robert <khazan21@gmail.com>
2023-08-28 15:35:07 -0500

    added log out button on home screen with useContext :)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,44 @@
-import React from 'react'
-import Games from './Games'
+import React, { useContext } from 'react';
+import Games from './Games';
 import Title from './Title';
-import { UserContext } from '../context/user.js';
-import { useContext } from 'react';
+import { UserContext } from '../context/user';
+import { useHistory } from 'react-router-dom';
 
 function Home() {
+  const { user, setUser } = useContext(UserContext);
+  const history = useHistory();
 
-  const { user } = useContext(UserContext);
-  console.log(user);
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('/logout', {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        setUser(null);
+        history.push('/');
+      } else {
+        console.log('Error logging out:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
 
   return (
     <div>
+      <div className='top-right'>
+        {user ? (
+          <button className='logoutBtn' onClick={handleLogout}>
+            Logout
+          </button>
+        ) : null}
+      </div>
       <Title display={'Game Reviewer'}></Title>
       <Games></Games>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
